40561218c0f330e1a8f365669a0e4d4401fcbe2b
emilia-joha <emilia.joha@live.se>
2022-12-16 15:06:20 +0100

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -1,19 +1,19 @@
 function getTokens() {
         const params= new URLSearchParams({
+            response_type:"code",
             client_id :'ee0bdca0269146eaa8345787f079f9d2',
             scope :'user-follow-read',
             rederect_uri : 'http://localhost:5000/callback',
-            response_type:"code"
+            
         })
        
 
         $.ajax({
             method: "GET",
-            url: "https://accounts.spotify.com/authorize?response_type=code&client_id=ee0bdca0269146eaa8345787f079f9d2&scope=user-follow-read&redirect_uri=http://localhost:5000/callback",
-        
+            url: "https://accounts.spotify.com/authorize?client_id=" + params.toString(),
+            headers: {"Accept": "application/json"}
         })
         .done(function(result){
             console.log(JSON.stringify(result));
     });
-   
 }
