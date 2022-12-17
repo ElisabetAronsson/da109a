a276f849b440c956a5fbf8bf0a27c39b285bf5f9
emilia-joha <emilia.joha@live.se>
2022-12-17 12:49:59 +0100

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -1,18 +1,22 @@
 function getTokens() {
-        const params= new URLSearchParams({
-            response_type:"code",
-            client_id :'ee0bdca0269146eaa8345787f079f9d2',
-            scope :'user-follow-read',
-            redirect_uri : 'http://localhost:5000/callback',
-        });
-       
-        
-        $.ajax({
-            method: "GET",
-            url: "https://accounts.spotify.com/authorize?"+ params.toString(),
-            headers: {"Accept": "application/json"}
-        })
-        .done(function(result){
-            console.log(JSON.stringify(result));
-    });
+  const params= new URLSearchParams({
+      response_type:"code",
+      client_id :'ee0bdca0269146eaa8345787f079f9d2',
+      scope :'user-follow-read',
+      redirect_uri : 'http://localhost:5000/callback',
+  });
+
+  const url = "https://accounts.spotify.com/authorize?"+ params.toString();
+  location.href = url;
+
+      
+
+  // $.ajax({
+  //     method: "GET",
+  //     url: "https://accounts.spotify.com/authorize?"+ params.toString(),
+  //     headers: {"Accept": "application/json"}
+  // })
+  // .done(function(result){
+  //     console.log(JSON.stringify(result));
+  // });
 }
