d5fd909bfdcc0d80e78c440c9e95703064436a5f
emilia-joha <emilia.joha@live.se>
2022-12-19 12:11:37 +0100

    Update spotify-auth.js

--- src/main/resources/public/script/spotify-auth.js
+++ src/main/resources/public/script/spotify-auth.js
@@ -21,7 +21,7 @@ function getToken(){
 $(document).ready(function(){
     let scope = 'user-follow-read';
     let clientID = '7a6d44b7b4c7435f9ed4340b9e613395';
-    let redirect_uir = 'http://localhost:8888/callback';
+    let redirect_uir = 'http://localhost:8888/';
     let response_type = 'token';
     
     //window.open(`https://accounts.spotify.com/authorize?client_id=${clientID}&redirect_uri=${redirect_uir}&response_type=${response_type}&scope=${scope}`,'_self')
