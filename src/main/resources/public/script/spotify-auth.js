885021822b66bd003d7cd8676b79126b60229fad
emilia-joha <emilia.joha@live.se>
2022-12-19 12:02:38 +0100

    ändringar i rauts

--- src/main/resources/public/script/spotify-auth.js
+++ src/main/resources/public/script/spotify-auth.js
@@ -17,7 +17,7 @@ function getToken(){
     }
 }
 
-$(document).ready(function(){
+function hej() {
     let scope = 'user-follow-read';
     let clientID = '7a6d44b7b4c7435f9ed4340b9e613395';
     let redirect_uir = 'http://localhost:8888/callback';
@@ -28,7 +28,7 @@ $(document).ready(function(){
 
     let token = getToken();
    
-})
+}
 
 
 
