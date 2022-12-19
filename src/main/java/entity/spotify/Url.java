eabbff34a25f1410980a4f4bc2584b3439ec3d77
Hugo Lindstedt <hugolindstedt@outlook.com>
2022-12-19 15:33:23 +0100

    spotify api

---
+++ src/main/java/entity/spotify/Url.java
@@ -0,0 +1,17 @@
+package entity.spotify;
+
+public class Url {
+    private String spotify;
+
+    public Url() {
+    }
+
+    public String getSpotify() {
+        return spotify;
+    }
+
+    public void setSpotify(String spotify) {
+        this.spotify = spotify;
+    }
+}
+
