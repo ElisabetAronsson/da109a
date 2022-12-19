b3f25b13f283e0763e8de3f805e8fccb44048b17
Hugo Lindstedt <hugolindstedt@outlook.com>
2022-12-19 11:09:41 +0100

    removed files

--- src/main/java/Testfile.java
+++
@@ -1,9 +0,0 @@
-import io.javalin.Javalin;
-
-public class Testfile {
-    public static void main(String[] args) {
-        Javalin app = Javalin.create().start(5000);
-
-        //SpotifyAuth spotify = new SpotifyAuth();
-    }
-}
