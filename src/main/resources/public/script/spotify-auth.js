234d0b2c7ef33ed40fbee61fedc21d6c0426cf2a
emilia-joha <emilia.joha@live.se>
2022-12-19 12:33:56 +0100

    Update spotify-auth.js

--- src/main/resources/public/script/spotify-auth.js
+++ src/main/resources/public/script/spotify-auth.js
@@ -1,22 +1,15 @@
 function getToken(){
     let params = (new URL(location.href.replace('#','?'))).searchParams;
     let token = params.get('access_token');
-    window.localStorage.setItem('access_token',token)
+    let hej = window.localStorage.setItem('access_token',token)
 
-    function getData(url = '') {
-        const response =  fetch(url, {
-            method : 'GET',
-            headers: {
-                'authorization': 'Bearer '+ token
-            }
-        });
-        
-        const artists =  response.json();
-        console.log(artists)
+    console.log(params)
+    console.log(token)
+    console.log(hej)
         return artists
     
     }
-}
+    
 function login(){
     let scope = 'user-follow-read';
     let clientID = '7a6d44b7b4c7435f9ed4340b9e613395';
@@ -24,7 +17,8 @@ function login(){
     let response_type = 'token';
     
     //window.open(`https://accounts.spotify.com/authorize?client_id=${clientID}&redirect_uri=${redirect_uir}&response_type=${response_type}&scope=${scope}`,'_self')
-    window.location.href = `https://accounts.spotify.com/authorize?client_id=${clientID}&redirect_uri=${redirect_uir}&response_type=${response_type}&scope=${scope}`; 
+    window.location.href = `https://accounts.spotify.com/authorize?client_id=${clientID}&redirect_uri=${redirect_uir}&response_type=${response_type}&scope=${scope}`;
+ 
 }
 
 
