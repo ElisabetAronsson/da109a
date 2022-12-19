089c33cde683c08684141d66070acb922cc8543d
Hugo Lindstedt <hugolindstedt@outlook.com>
2022-12-19 15:26:23 +0100

    spotify api

--- src/main/java/Controller.java
+++ src/main/java/Controller.java
@@ -1,4 +1,9 @@
+import com.fasterxml.jackson.core.type.TypeReference;
+import com.fasterxml.jackson.databind.DeserializationFeature;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import com.google.gson.Gson;
+import entity.spotify.Example;
+import entity.spotify.Items;
 import io.javalin.http.Context;
 
 import java.io.IOException;
@@ -7,22 +12,26 @@ import java.net.URISyntaxException;
 import java.net.http.HttpClient;
 import java.net.http.HttpRequest;
 import java.net.http.HttpResponse;
+import java.util.List;
 
 public class Controller {
 
-    static final Gson gson = new Gson();
+    static final ObjectMapper mapper = new ObjectMapper()
+            .configure(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES, false);
 
     public static void getFollowing(Context context) throws URISyntaxException, IOException, InterruptedException {
 
         String token = context.req().getHeader("Authorization");
         HttpRequest getRequest = HttpRequest.newBuilder()
-                .uri(new URI("https://api.spotify.com/v1/me/following?type=artist&limit=10"))
+                .uri(new URI("https://api.spotify.com/v1/me/following?type=artist&limit=2"))
                 .header("Content-Type","application/json")
                 .header("Authorization", token)
                 .build();
 
         HttpClient httpClient = HttpClient.newHttpClient();
         HttpResponse<String> getResponse = httpClient.send(getRequest, HttpResponse.BodyHandlers.ofString());
-        context.result(getResponse.body());
+
+        Example itemsList = mapper.readValue(getResponse.body(), Example.class);
+        context.result(mapper.writeValueAsString(itemsList));
     }
 }
