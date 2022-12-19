ae74a27f432b870bfc7374bd7957c71695fbbb38
emilia-joha <emilia.joha@live.se>
2022-12-19 12:19:55 +0100

    test

--- src/main/resources/public/script/spotify-auth.js
+++ src/main/resources/public/script/spotify-auth.js
@@ -17,20 +17,15 @@ function getToken(){
     
     }
 }
-
-$(document).ready(function(){
+function login(){
     let scope = 'user-follow-read';
     let clientID = '7a6d44b7b4c7435f9ed4340b9e613395';
-    let redirect_uir = 'http://localhost:8888/';
+    let redirect_uir = 'http://localhost:8888/callback';
     let response_type = 'token';
     
     //window.open(`https://accounts.spotify.com/authorize?client_id=${clientID}&redirect_uri=${redirect_uir}&response_type=${response_type}&scope=${scope}`,'_self')
-    window.location.href = `https://accounts.spotify.com/authorize?client_id=${clientID}&redirect_uri=${redirect_uir}&response_type=${response_type}&scope=${scope}`;
-
-    let token = getToken();
-   
-})
-
+    window.location.href = `https://accounts.spotify.com/authorize?client_id=${clientID}&redirect_uri=${redirect_uir}&response_type=${response_type}&scope=${scope}`; 
+}
 
 
 
