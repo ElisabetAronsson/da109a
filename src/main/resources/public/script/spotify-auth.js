f8a7975852c859256aa266fe8cd4e1dc6f8c3cd2
emilia-joha <emilia.joha@live.se>
2022-12-20 12:22:17 +0100

    Update spotify-auth.js

--- src/main/resources/public/script/spotify-auth.js
+++ src/main/resources/public/script/spotify-auth.js
@@ -1,3 +1,14 @@
+function login(){
+    let scope = 'user-follow-read';
+    let clientID = '7a6d44b7b4c7435f9ed4340b9e613395';
+    let redirect_uir = 'http://localhost:8888/callback';
+    let response_type = 'token';
+    
+    //window.open(`https://accounts.spotify.com/authorize?client_id=${clientID}&redirect_uri=${redirect_uir}&response_type=${response_type}&scope=${scope}`,'_self')
+    window.location.href = `https://accounts.spotify.com/authorize?client_id=${clientID}&redirect_uri=${redirect_uir}&response_type=${response_type}&scope=${scope}`;
+ 
+}
+
 function getToken(){
     let searchObject ={};
     searchObject.location=$("#locationForm input[name=location]").val()
@@ -23,7 +34,7 @@ function getToken(){
         postData(searchObject)
         
     };
-};
+}
 
 function postData(data){
     $.ajax({
@@ -39,13 +50,4 @@ function postData(data){
 }
     
 
-function login(){
-    let scope = 'user-follow-read';
-    let clientID = '7a6d44b7b4c7435f9ed4340b9e613395';
-    let redirect_uir = 'http://localhost:8888/callback';
-    let response_type = 'token';
-    
-    //window.open(`https://accounts.spotify.com/authorize?client_id=${clientID}&redirect_uri=${redirect_uir}&response_type=${response_type}&scope=${scope}`,'_self')
-    window.location.href = `https://accounts.spotify.com/authorize?client_id=${clientID}&redirect_uri=${redirect_uir}&response_type=${response_type}&scope=${scope}`;
- 
-}
+
