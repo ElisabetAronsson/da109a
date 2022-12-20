060d2a2ee7f917f812ee302c35c53d7f543ed02f
Hugo Lindstedt <hugolindstedt@outlook.com>
2022-12-20 12:38:57 +0100

    spotify json deserialize

--- src/main/java/Controller.java
+++ src/main/java/Controller.java
@@ -8,7 +8,7 @@ import java.net.URISyntaxException;
 import java.net.http.HttpClient;
 import java.net.http.HttpRequest;
 import java.net.http.HttpResponse;
-import java.util.List;
+
 
 public class Controller {
 
