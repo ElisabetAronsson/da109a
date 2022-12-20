410019f45b2d6c0e1acb38b56491e867b0a8e6f9
emilia-joha <emilia.joha@live.se>
2022-12-20 10:43:49 +0100

    wip

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,11 +1,15 @@
 import io.javalin.Javalin;
 import io.javalin.http.staticfiles.Location;
 
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
+
 import static io.javalin.apibuilder.ApiBuilder.get;
 import static io.javalin.apibuilder.ApiBuilder.path;
 
 public class Main {
     public static void main(String[] args) {
+        Main runner=new Main();
         var app = Javalin.create( javalinConfig -> {
             javalinConfig.staticFiles.add("/public/", Location.CLASSPATH);
             javalinConfig.staticFiles.add("/public/css/", Location.CLASSPATH);
@@ -14,6 +18,7 @@ public class Main {
                 })
                 .get("/", ctx -> ctx.redirect("index.html"))
                 .get("/callback", ctx -> ctx.redirect("callback.html"))
+                .post("/callback", ctx -> {runner.data(ctx)})
                 .get("/listArtists", ctx -> ctx.redirect("listArtists.html"))
                 .start(8888);
 
@@ -21,5 +26,14 @@ public class Main {
         app.routes(() -> {
            path("/v1/artists/following", () -> get(Controller::getFollowing));
         });
+    }
+
+    private void data(context ctx) {
+
+        data=gson.fromJson(ctx.body())
+        try(Scanner scan=new Scanner(System.in)){
+
+            //In Java, write to console
+              System.out.println(data);
     }
 }
