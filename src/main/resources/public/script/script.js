c5b2ac9cf8f20fc1264cf20bc7ea5d66357dda38
emilia-joha <emilia.joha@live.se>
2022-12-20 19:04:14 +0100

    css

--- src/main/resources/public/script/script.js
+++ src/main/resources/public/script/script.js
@@ -54,7 +54,7 @@ $(document).ready(function(){
         const artists=JSON.parse(data).artists.items
     
         for(i=0; i<artists.length; i++){
-            html="<li id='artist_" + i + "'> <div> <img class='artistListImage' src='" + artists[i]["images"][0]["url"] + "'> <h4>" + artists[i]["name"] + "</h4> </div> </li>" // key:n kan behöva ändras
+            html="<div id='artist_" + i + "'> <img class='artistListImage' src='" + artists[i]["images"][0]["url"] + "'> <h4>" + artists[i]["name"] + "</h4> </div>" // key:n kan behöva ändras
             listArtists.append(html);
 
             $("#artist_" + i).click(fetchEvent(artists[i]["name"]));
