f2791c7ffe98bf0c3fd2e28d021403d74b1944fa
emilia-joha <emilia.joha@live.se>
2022-12-30 16:49:37 +0100

    visar artisten och eventet

--- src/main/resources/public/script/script.js
+++ src/main/resources/public/script/script.js
@@ -1,6 +1,6 @@
 function fetchEvents(){
         let params = (new URL(location.href)).searchParams;
-        let artistName = params.get('artist').replace("%20", "_")
+        let artistName = params.get('artist').replace("%20", " ")
 
         const artists= JSON.parse(window.localStorage.getItem('eventAndArtists'))
         
@@ -16,9 +16,9 @@ function fetchEvents(){
                     for(y=0; y<artists[i]["events"].length; y++){
                         let date = artists[i]["events"][y]["datetime_utc"].split("T")
 
-                        const eventName =  artists[i]["events"][y]["short_title"]
+                        let eventName =  artists[i]["events"][y]["short_title"]
     
-                        html="<div id='event_" + i + "'> <a href='eventAndArtist.html?artist=" + artistName + "?event=" + eventName + "'> <h4>" + artists[i]["events"][y]["short_title"] + "</h4> <p>" + artists[i]["events"][y]["venue"]["city"] + "</p> <p>" + date[0] + "</p></a></div>"
+                        html="<div id='event_" + i + "'> <a href='eventAndArtist.html?artist=" + artistName +  "&event=" + eventName + "'> <h4>" + artists[i]["events"][y]["short_title"] + "</h4> <p>" + artists[i]["events"][y]["venue"]["city"] + "</p> <p>" + date[0] + "</p></a></div>"
                         listEvent.append(html);
                     }
                 }};    
@@ -27,43 +27,36 @@ function fetchEvents(){
 
 function fetchInfo(){
     let params = (new URL(location.href)).searchParams;
-    let artistName = params.get('artist').replace("%20", "_")
-    let eventName = params.get("event").replace("%20", "_")
 
-    location.href = "eventAndArtist.html";
-
-
+    let artistName = params.get('artist').replace("%20", " ");
+    let eventName = params.get('event').replace("%20", " ");
+   
     const artists= JSON.parse(window.localStorage.getItem('eventAndArtists'))
         
-    listEvent=$("#eventsList");
+    for(i=0; i<artists.length; i++){
 
-    
-        for(i=0; i<artists.length; i++){
+        if(artists[i]["name"]== artistName){
 
-            if(artists[i]["name"]== artistName){
+            for(y=0; y<artists[i]["events"].length; y++){
 
-                for(y=0; y<artists[i]["events"].length; y++){
+                if(artists[i]["events"][y]["short_title"] == eventName){
 
-                    if(artists[i]["events"][y]["short_title"] == eventName){
+                    let date = artists[i]["events"][y]["datetime_utc"].split("T")
 
-                        let date = artists[i]["events"][y]["datetime_utc"].split("T")
+                    $("#artistName").text(artistName);
+                    $("#artistImage").html("<img id='eventArtistImage' src='" + artists[i]["images"][0]["url"] + "'>");
+                    $("#artistInfo") //wikipedia info
+            
+                    $("#eventName").text(eventName);
+                    $("#eventWhere").text(artists[i]["events"][y]["venue"]["city"]);
+                    $("#eventDate").text(date[0]);
+                    $("#eventTime").text(date[1])
+                    $("#eventLocation").text(artists[i]["events"][y]["venue"]["name"]);
 
-                        $("#artistName").text(artistName);
-                        $("#artistImage").html("<img src='" + artists["image"] + "'>");
-                        $("#artistInfo") //wikipedia info
-                
-                        $("#eventName").text(eventName);
-                        $("#eventWhare").text(artists[i]["events"][y]["venue"]["city"]);// key:n kan behöva ändras
-                        $("#eventDate").text(date[0]);
-                        $("#eventTime").text(date[1])
-                        $("#eventLocation").text(artists[i]["events"][y]["venue"]["name"]);// key:n kan behöva ändras
-
-                    }
                 }
-            }};   
-
-       
-    
+            }
+        }
+    };    
 }
 
 function fetchArtists(){
@@ -76,8 +69,7 @@ function fetchArtists(){
         listArtists=$("#artistsList");
 
         const artists=JSON.parse(data).items
-        console.log(artists)
-    
+        
         for(i=0; i<artists.length; i++){
             html="<div id='artist_" + i + "'> <a href='listEvent.html?artist=" + artists[i]['name'] + "'> <img class='artistListImage' src='" + artists[i]["images"][0]["url"] + "'> <h4>" + artists[i]["name"] + "</h4> </a></div>"
             listArtists.append(html);
