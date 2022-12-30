644dc0f1bb31c2728fef5e3fcb5194913b795d42
ElisabetAronsson <96416333+ElisabetAronsson@users.noreply.github.com>
2022-12-30 12:35:05 +0100

    New class

---
+++ src/main/java/entity/spotify/ArtistsWrapper.java
@@ -0,0 +1,14 @@
+package entity.spotify;
+import entity.spotify.Artists;
+
+public class ArtistsWrapper {
+    private Artists artists;
+
+    public Artists getArtists() {
+        return artists;
+    }
+
+    public void setArtists(Artists artists) {
+        this.artists = artists;
+    }
+}
