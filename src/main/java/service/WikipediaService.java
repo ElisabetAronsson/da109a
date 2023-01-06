f20cbcd9c2bea01b8640cb7c8438356185ee7fc7
hugolind <hugolindstedt@outlook.com>
2023-01-06 14:17:31 +0100

    tagit bort ej använda imports

--- src/main/java/service/WikipediaService.java
+++ src/main/java/service/WikipediaService.java
@@ -1,23 +1,11 @@
 package service;
 
-import com.fasterxml.jackson.databind.DeserializationFeature;
-import com.fasterxml.jackson.databind.JsonNode;
-import com.fasterxml.jackson.databind.ObjectMapper;
-import entity.seatgeek.Events;
-import entity.spotify.Artists;
-import entity.spotify.ArtistsWrapper;
-import entity.spotify.Items;
 import entity.wikipedia.ExtractWrapper;
-import io.javalin.http.Context;
 import org.apache.http.HttpEntity;
 import org.apache.http.HttpResponse;
-import org.apache.http.client.HttpClient;
-import org.apache.http.client.config.CookieSpecs;
-import org.apache.http.client.config.RequestConfig;
 import org.apache.http.client.methods.HttpGet;
 import org.apache.http.impl.client.CloseableHttpClient;
 import org.apache.http.impl.client.HttpClientBuilder;
-import org.apache.http.impl.client.HttpClients;
 import org.apache.http.util.EntityUtils;
 import java.io.IOException;
 import java.net.URISyntaxException;
